3e6c90b4ddab5c3034906f21fa193229fe21c2ea
Amune Israel <iharmuneh@gmail.com>
2024-04-01 04:08:55 +0100

    added error handler in the crawl page handler

--- src/craw.ts
+++ src/craw.ts
@@ -1,4 +1,27 @@
 import { JSDOM } from "jsdom";
+import axios from "axios";
+export const crawlPage = async (currentUrl: string) => {
+  try {
+    console.log("actively crawling the page", currentUrl);
+    const res = await axios.get(currentUrl);
+    const contentType = res.headers["content-type"];
+    
+    if (res.status > 399) {
+      console.log(`error in fetching with status code:
+      ${res.status} on page ${currentUrl}`);
+    }
+    if (!contentType.includes('text/html')) {
+      console.log(
+        `non html response,content type:${contentType} on page ${currentUrl}`
+      );
+      return ;
+    }
+    console.log(res.data);
+  } catch (error) {
+    console.log(`error fetching ${error.message} on page: ${currentUrl}`);
+  }
+};
+
 export const normalizeUrl = (urlParams: string) => {
   const url = new URL(urlParams);
   const hostPath = `${url.hostname}${url.pathname}`;
@@ -15,22 +38,19 @@ export const getUrlsFromHtml = (htmlBody, baseUrl: string) => {
 
   for (const linkElement of linkElements) {
     if (linkElement.href.slice(0, 1) === "/") {
-        try {
-            const urlObj = new URL(`${baseUrl}${linkElement.href}`);
-            urls.push(urlObj.href);
-        } catch (error) {
-            console.log(error.message)
-        }
-    
+      try {
+        const urlObj = new URL(`${baseUrl}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (error) {
+        console.log(error.message);
+      }
     } else {
-        try {
-            const urlObj = new URL(linkElement.href);
-            urls.push(urlObj.href);
-        } catch (error) {
-            console.log(error.message)
-        }
-     
-    
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (error) {
+        console.log(error.message);
+      }
     }
   }
   return urls;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello world")
+import { crawlPage } from "./craw";
+
+async function main() {
+    if(process.argv.length<3){
+      console.log("no website provide");
+      process.exit(1)
+    }
+    if(process.argv.length>3){
+      console.log("too many command line arg")
+      process.exit(1)
+    };
+    const baseUrl = process.argv[2];
+    console.log("starting web crawl");
+    await crawlPage(baseUrl);
+}
+main();
